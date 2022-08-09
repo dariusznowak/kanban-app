239fd62d0d23fcb889e3768d77182f9085ae6c34
Dariusz Nowak <dariusz.nowak012@gmail.com>
2022-08-10 01:03:16 +0200

    Developing layout

--- src/components/TaskTable.js
+++ src/components/TaskTable.js
@@ -2,7 +2,23 @@ import React from "react";
 import "../styles/TaskTable.scss";
 
 const TaskTable = () => {
-  return <div className="task-table">Task table</div>;
+  return (
+    <div className="task-table">
+      <div className="taskTable-header">
+        <p>To do</p>
+        <button className="taskTable-actionsBtn">...</button>
+      </div>
+      <div className="taskTable-container">
+        <div className="taskTable-singleTask">taskTable-singleTask</div>
+        <div className="taskTable-singleTask">taskTable-singleTask</div>
+        <div className="taskTable-singleTask">taskTable-singleTask</div>
+        <div className="taskTable-singleTask">taskTable-singleTask</div>
+      </div>
+      <div className="taskTable-footer">
+        <button className="taskTable-addCardBtn">+ Add card</button>
+      </div>
+    </div>
+  );
 };
 
 export default TaskTable;
